b160e8139bf1b267ddc31213f20d2aa97e3762c6
HanYa <101889208+CodingHanYa@users.noreply.github.com>
2023-01-18 00:04:39 +0800

    interface_test
    version 1.0

---
+++ test_steady_pond_interface.cpp
@@ -0,0 +1,124 @@
+#include "../hipe.h"
+using namespace hipe;
+
+// A synchronous IO stm
+util::SyncStream stm;
+
+void foo1() {
+    stm.print("call foo1");
+}
+
+void foo2(std::string name) {
+    stm.print(name, " call foo2");
+}
+
+struct Functor {
+    void operator()() {
+        stm.print("functor executed");
+    }
+};
+
+void test_submit(SteadyThreadPond& pond) 
+{
+    stm.print("\n", util::boundary('=', 15), util::strong("submit"), util::boundary('=', 16));
+
+    // no return
+    pond.submit(foo1);  // function pointer
+    pond.submit([]{stm.print("HanYa say hello world");}); // lambda
+    pond.submit(std::bind(foo2, "HanYa"));  // std::function
+
+    // if you need many returns
+    int n = 3;
+    HipePromiseVector<int> promises(n);
+    HipeFutureVector<int>  futures(n);
+    util::futureBindPromise(futures, promises);
+
+    for (int i = 0; i < n; ++i) {
+        pond.submit([&, i] { promises[i].set_value(i); });
+    }
+    for (int i = 0; i < n; ++i) {
+        stm.print("get return ", futures[i].get());
+    }
+
+    // or
+    HipeFuture<double> fut;
+    HipePromise<double> pro;
+    util::futureBindPromise(fut, pro);
+
+    pond.submit([&pro]{ pro.set_value(12.25); });
+    stm.print("get return ",fut.get());
+}
+
+void test_submit_by_batch(SteadyThreadPond& pond) 
+{
+    stm.print("\n", util::boundary('=', 11), util::strong("submit in batch"), util::boundary('=', 11));
+
+    // use util::block
+    int n = 2;
+    util::Block<HipeTask> blok(n);
+    for (int i = 0; i < n; ++i) { 
+        blok.add([i]{stm.print("block task ", i);});
+    }
+    pond.submitInBatch(blok, blok.element_numb());
+
+
+    // use std::vector , interface reset() from HipeTask
+    std::vector<HipeTask> vec(n);
+    for (int i = 0; i < n; ++i) {
+        vec[i].reset([i]{stm.print("vector task ", i);});
+    }
+    pond.submitInBatch(vec, vec.size());
+
+    // use another kind of submit by batch
+    pond.submit([]{ stm.print("same task submitted two times ");}, 2);
+    pond.waitForTasks();
+}
+
+void test_task_overflow() 
+{
+    stm.print("\n", util::boundary('=', 11), util::strong("task overflow"), util::boundary('=', 13));
+
+    // task capacity is 100 
+    SteadyThreadPond pond(10, 100);
+
+    pond.setRefuseCallBack([&]{
+        stm.print("Task overflow !");
+        auto blok = pond.pullOverFlowTasks();
+        stm.print("Losed ", blok.element_numb(), " tasks");
+    });
+
+    util::Block<HipeTask> my_block(101);
+    for (int i = 0; i < 101; ++i) {
+        my_block.add([]{util::sleep_for_milli(10);});
+    }
+
+    pond.submitInBatch(my_block, 101);
+
+}
+
+
+
+
+int main() 
+{
+    stm.print(util::title("Test SteadyThreadPond", 10));
+
+    // unlimited task capacity
+    SteadyThreadPond pond(8);
+
+    // unlimited task capacity pond can't set refuse callback
+    // pond.setRefuseCallBack([]{stm.print("task overflow!!!");});
+
+    test_submit(pond);
+    util::sleep_for_seconds(1);
+
+    test_submit_by_batch(pond);
+    util::sleep_for_seconds(1);
+
+    test_task_overflow(); 
+    util::sleep_for_seconds(1);
+   
+
+    stm.print("\n", util::title("End of the test", 5));
+
+}
