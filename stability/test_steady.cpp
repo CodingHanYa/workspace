00360d6acf03f91c05aeb9680fef5c5688416a58
HanYa <1848395727@qq.com>
2023-02-11 19:09:39 +0800

    chore(test_balance, test_steady)做更多测试，降低每次测试的任务数

--- stability/test_steady.cpp
+++ stability/test_steady.cpp
@@ -6,7 +6,7 @@ int main() {
     pond.enableStealTasks(4);
 
     std::atomic_int var(0);
-    int each_task_nums = 100000;
+    int each_task_nums = 10000;
 
     for (int i = 0; i < each_task_nums; ++i) {
         pond.submit([&] { var++; });
